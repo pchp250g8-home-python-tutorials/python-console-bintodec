cee6faf70eeccc211d6309377b6b3bfb6bfe3d87
Alexander <pchp250g8@tutamail.com>
2026-02-16 20:58:31 +0300

    Created Project BinToDec

---
+++ BinToDec.py
@@ -0,0 +1,22 @@
+#--conding:utf-8--
+import math
+import os
+import regex
+
+nDecNum = 0
+nBinPower = 1
+os.system("cls")
+print("Input a binary number:")
+strLine = input()
+nStrLen = len(strLine)
+oMatches = regex.findall("[0-1]",strLine)
+nMathes = len(oMatches)
+bRightString = (nStrLen == nMathes)
+if(not bRightString):
+    print("Wrong binary number format!!!")
+    exit(0)
+for i in range(nStrLen):
+    nBinDigit = ord(strLine[nStrLen - 1 - i]) - ord('0')
+    nDecNum += (nBinDigit * nBinPower)
+    nBinPower *= 2;
+print(f"The decenary equivalent of the binary number {strLine} is {nDecNum}")
