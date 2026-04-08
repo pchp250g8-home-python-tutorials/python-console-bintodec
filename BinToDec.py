b4f61cc2b983e2680db526da82812702768dc6fa
Alexander <pchp250g8@tutamail.com>
2026-04-08 10:25:12 +0300

    Update BinToDec.py

--- BinToDec.py
+++ BinToDec.py
@@ -20,4 +20,4 @@ for i in range(nStrLen):
     nBinDigit = ord(strLine[nStrLen - 1 - i]) - ord('0')
     nDecNum += (nBinDigit * nBinPower)
     nBinPower *= 2
-print(f"The decenary equivalent of the binary number {strLine} is {nDecNum}")
+print(f"The decimal equivalent of the binary number {strLine} is {nDecNum}")
