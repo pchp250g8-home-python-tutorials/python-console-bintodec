e1dcdbc07fdfa8eb6f18ad0fc22f6626885b7a29
Alexander <pchp250g8@tutamail.com>
2026-04-13 16:45:49 +0300

    Fixed Source Code

--- BinToDec.py
+++ BinToDec.py
@@ -10,7 +10,7 @@ os.system("cls")
 print("Input a binary number:")
 strLine = input()
 nStrLen = len(strLine)
-nMaxBinLen = math.trunc(math.log(MAX_INT, 2))
+nMaxBinLen = math.ceil(math.log(MAX_INT, 2))
 oMatches = regex.fullmatch("^[0-1]+$", strLine)
 bRightString = (nStrLen < nMaxBinLen and oMatches is not None)
 if (not bRightString):
@@ -20,4 +20,4 @@ for i in range(nStrLen):
     nBinDigit = ord(strLine[nStrLen - 1 - i]) - ord('0')
     nDecNum += (nBinDigit * nBinPower)
     nBinPower *= 2
-print(f"The decenary equivalent of the binary number {strLine} is {nDecNum}")
+print(f"The decimal equivalent of the binary number {strLine} is {nDecNum}")
